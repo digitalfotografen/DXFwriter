b3288a47f94ad03b10490c254f09832e8d19afe5
Greg Bowler <greg.bowler@g105b.com>
2015-08-03 19:00:37 +0100

    Rename test

--- test/Unit/ObjectTypes.test.php
+++ test/Unit/ObjectTypes.test.php
@@ -1,7 +1,7 @@
 <?php
 namespace DXF;
 
-class Constructor_Test extends \PHPUnit_Framework_TestCase {
+class ObjectTypes_Test extends \PHPUnit_Framework_TestCase {
 
 /**
  * This is the most basic test, to assert that the Writer class is autoloaded
